254d88019c779f850b646c382c513cc56fe73827
leo vinogradov <leovinogradov@gmail.com>
2017-10-22 21:14:22 -0700

    account update

--- client/pages/Account.vue
+++ client/pages/Account.vue
@@ -35,6 +35,13 @@
 .cust-tile-1 > .list__tile > i {
   margin: auto;
 }
+.cust-btn-1 {
+  margin: 2px 8px;
+  min-width: 50px;
+}
+.cust-btn-1 .btn__content {
+  padding: 0;
+}
 .profile-tabs .tabs__item {
   color: #333 !important;
   padding: 0 16px;
@@ -56,6 +63,11 @@
   /* position: absolute;
   top: 25px;
   right: 15px; */
+}
+@media (min-width: 961px) {
+  .cust-tile-2 {
+    margin-bottom: 8px;
+  }
 }
 @media (max-width: 960px) {
   .profile-pic-cont {
@@ -86,11 +98,12 @@
         <v-tabs-items>
 
           <v-tabs-content :key="'Profile'" :id="'Profile'">
-            <v-layout class="layout-cont">
+            <section style="padding: 0; margin: 15px; width: auto;">
+            <v-layout>
               <v-flex xs12 sm8>
                 <h1>Firstname Lastname</h1>
                 <v-layout>
-                  <v-flex xs12 sm8 class="no-padding">
+                  <v-flex xs12 sm10 class="no-padding">
                     <v-list>
                       <v-list-tile v-if="!userdata.school" class="cust-tile-2 grey-color">
                         <v-list-tile class="cust-tile-1">
@@ -98,14 +111,25 @@
                         </v-list-tile>
                         <v-list-tile-content>
                           <!--<v-list-tile-title>Add school info</v-list-tile-title>-->
-                          <v-text-field
-                            class="no-padding no-underline"
-                            name="input-1-3"
-                            label="Add school info"
-                            single-line
-                          ></v-text-field>
+                          <v-layout style="width: 100%">
+                          <v-flex xs10 class="no-padding">
+                            <v-text-field
+                              v-model="updateSchool"
+                              class="no-padding no-underline"
+                              name="input-1-3"
+                              label="Add school info"
+                              single-line
+                            ></v-text-field>
+                          </v-flex>
+                          <v-flex xs2 v-show="updateSchool" class="no-padding">
+                            <v-btn small  center class="cust-btn-1" @click="saveSchool">
+                              Save
+                            </v-btn>
+                          </v-flex>
+                        </v-layout>
                         </v-list-tile-content>
                       </v-list-tile>
+
                       <v-list-tile v-if="userdata.school" class="cust-tile-2">
                         <v-list-tile class="cust-tile-1">
                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
@@ -114,23 +138,66 @@
                           <v-list-tile-title>{{ userdata.school }}</v-list-tile-title>
                         </v-list-tile-content>
                       </v-list-tile>
+
+                      <v-list-tile v-if="userdata.school && !userdata.degree" class="cust-tile-2 grey-color">
+                        <v-list-tile class="cust-tile-1">
+                           <i class="fa fa-plus-square-o" aria-hidden="true"></i>
+                        </v-list-tile>
+                        <v-list-tile-content>
+                          <v-layout style="width: 100%">
+                            <v-flex xs10 class="no-padding">
+                              <v-text-field
+                                v-model="updateDegree"
+                                class="no-padding no-underline"
+                                name="input-2"
+                                label="Add degree"
+                                single-line
+                              ></v-text-field>
+                            </v-flex>
+                            <v-flex xs2 v-show="updateDegree" class="no-padding">
+                              <v-btn small  center class="cust-btn-1" @click="saveDegree">
+                                Save
+                              </v-btn>
+                            </v-flex>
+                          </v-layout>
+                        </v-list-tile-content>
+                      </v-list-tile>
+
+                      <v-list-tile v-if="userdata.degree" class="cust-tile-2">
+                        <v-list-tile class="cust-tile-1">
+                           <i class="fa fa-book" aria-hidden="true"></i>
+                        </v-list-tile>
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{ userdata.degree }}</v-list-tile-title>
+                        </v-list-tile-content>
+                      </v-list-tile>
+
                       <v-list-tile v-if="!userdata.email" class="cust-tile-2 grey-color">
                         <v-list-tile class="cust-tile-1">
                             <i class="fa fa-plus-square-o" aria-hidden="true"></i>
                         </v-list-tile>
                         <v-list-tile-content>
-                          <!--<v-list-tile-title>Add email</v-list-tile-title>-->
-                          <v-text-field
-                            class="no-padding no-underline"
-                            name="input-1-3"
-                            label="Add personal email"
-                            single-line
-                          ></v-text-field>
+                          <v-layout style="width: 100%">
+                            <v-flex xs10 class="no-padding">
+                              <v-text-field
+                                v-model="updateEmail"
+                                class="no-padding no-underline"
+                                name="input-3"
+                                label="Add personal email"
+                                single-line
+                              ></v-text-field>
+                            </v-flex>
+                            <v-flex xs2 v-show="updateEmail" class="no-padding">
+                              <v-btn small  center class="cust-btn-1" @click="saveEmail">
+                                Save
+                              </v-btn>
+                            </v-flex>
+                          </v-layout>
                         </v-list-tile-content>
                       </v-list-tile>
                       <v-list-tile v-if="userdata.email" class="cust-tile-2">
                         <v-list-tile class="cust-tile-1">
-                           <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                           <i class="fa fa-envelope-o" aria-hidden="true"></i>
                         </v-list-tile>
                         <v-list-tile-content>
                           <v-list-tile-title>{{ userdata.email }}</v-list-tile-title>
@@ -145,7 +212,24 @@
                 </div>
               </v-flex>
             </v-layout>
-
+            <v-divider style="margin: 0 15px; width: auto;"></v-divider>
+            <v-layout>
+              <v-flex xs12 sm6>
+                <h3>Resume</h3>
+                <v-btn small @click="next">
+                  Add resume
+                </v-btn>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <h3 style="text-align: right">Organizations</h3>
+                <div class="float-right">
+                  <v-btn outline small fab class="grey--text lighten-2">
+                    <v-icon>add</v-icon>
+                  </v-btn>
+                </div>
+              </v-flex>
+            </v-layout>
+            </section>
           </v-tabs-content>
 
           <v-tabs-content :key="'Resume'" :id="'Resume'">
@@ -174,13 +258,29 @@
         active: null,
         userdata: {
           school: null,
+          degree: null,
           email: null,
         },
+        updateSchool: '',
+        updateEmail: '',
+        updateDegree: '',
       };
     },
     methods: {
       next() {
         this.active = this.tabs[(this.tabs.indexOf(this.active) + 1) % this.tabs.length];
+      },
+      saveSchool() {
+        this.userdata.school = this.updateSchool;
+        this.updateSchool = '';
+      },
+      saveDegree() {
+        this.userdata.degree = this.updateDegree;
+        this.updateDegree = '';
+      },
+      saveEmail() {
+        this.userdata.email = this.updateEmail;
+        this.updateEmail = '';
       },
     },
   };
