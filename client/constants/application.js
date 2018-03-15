a459fc46ca096fb482dd3882b1978bf3395fc2be
Chau <ChauTNguyen96@gmail.com>
2018-03-15 14:18:28 -0700

    fix application constants (accepted/rejected)

--- client/constants/application.js
+++ client/constants/application.js
@@ -2,8 +2,8 @@ export default {
   status: {
     submitted: 'Unseen by employer',
     opened: 'Seen by employer',
-    accepted: `Your application was accepted! Check your email, the employer will contact you shortly.`,
-    rejected: 'No longer under consideration',
+    accepted: 'Your application was accepted! Check your email, the employer will contact you shortly.',
+    rejected: 'Application rejected',
     withdrawn: 'Application withdrawn',
   },
 };
