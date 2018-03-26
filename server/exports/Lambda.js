bd8a77d2311286c23ede012f65bd206fb2d316c7
Zhaofeng Li <hello@zhaofeng.li>
2018-03-26 23:08:29 +0000

    Lambda.js: Support triggering the scheduler via Lambda

--- server/exports/Lambda.js
+++ server/exports/Lambda.js
@@ -1,8 +1,23 @@
 import AwsServerlessExpress from 'aws-serverless-express';
+import Scheduler from '@/Scheduler';
 
 export default class Lambda {
   static get(app) {
     const server = AwsServerlessExpress.createServer(app.callback());
-    return (event, context) => AwsServerlessExpress.proxy(server, event, context);
+    return (event, context) => {
+      if (event.scheduler) {
+        // Scheduler trigger
+        Scheduler.trigger().then(() => {
+          context.succeed({
+            success: true,
+          });
+        }).catch((e) => {
+          context.fail(e);
+        });
+      } else {
+        // Web request
+        AwsServerlessExpress.proxy(server, event, context);
+      }
+    };
   }
 }
