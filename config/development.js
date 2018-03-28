2ed934e8c4d1a56b9acdafa22b0090392a304c7d
Zhaofeng Li <hello@zhaofeng.li>
2018-03-28 18:49:01 +0000

    config/development.js: Restore localhost endpoint

--- config/development.js
+++ config/development.js
@@ -7,8 +7,7 @@
 
 const base = require('./production.js');
 
-// base.serverUrl = 'http://localhost:3000';
-base.serverUrl = 'https://nm9k39t3a7.execute-api.us-west-1.amazonaws.com/testprod';
+base.serverUrl = 'http://localhost:3000';
 base.private.database = 'mongodb://localhost/kunvet-dev';
 base.private.files.defaultStorage = 'local';
 base.private.files.s3.bucket = 'kunvet-dev';
