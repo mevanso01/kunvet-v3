da6ca1d4fe4bf7683473f10f59202b34a798d78f
Zhaofeng Li <hello@zhaofeng.li>
2018-03-28 21:27:56 +0000

    A new building experience
    Run `yarn build` to try it out

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+/* eslint-disable arrow-body-style */
+
+import gulp from 'gulp';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
+import shell from 'gulp-shell';
+import archiver from 'archiver';
+
+import colors from 'colors/safe';
+import fs from 'fs';
+import path from 'path';
+
+import clientConfig from './build/webpack.config.client';
+import serverConfig from './build/webpack.config.server';
+
+const progress = {};
+
+function redrawProgress() {
+  const formatted = Object.keys(progress).map((task) => {
+    const percent = progress[task].percent;
+    const message = progress[task].message;
+    const percentFormatted = colors.blue(`${percent}%`);
+    return `${task}: ${percentFormatted} ${message}`;
+  });
+  process.stderr.clearLine();
+  process.stderr.cursorTo(0);
+  process.stderr.write(formatted.join(' | '));
+}
+
+function setProgress(task, percent, message) {
+  progress[task] = {
+    percent,
+    message,
+  };
+  redrawProgress();
+}
+
+function processWebpackConfig(task, config) {
+  const newConfig = Object.assign({}, config);
+  newConfig.plugins.push(new webpack.ProgressPlugin((percentage, message) => {
+    const percent = Math.round(percentage * 100);
+    setProgress(task, percent, message);
+  }));
+  if (['development', 'production'].includes(process.env.NODE_ENV)) {
+    newConfig.mode = process.env.NODE_ENV;
+  }
+  return newConfig;
+}
+
+// Client SPA
+gulp.task('client', () => {
+  const config = processWebpackConfig('client', clientConfig);
+  return gulp.src('client/index.js')
+    .pipe(webpackStream(config, webpack))
+    .pipe(gulp.dest('dist/client'));
+});
+
+// Node.js Server
+gulp.task('server', () => {
+  const config = processWebpackConfig('server', serverConfig);
+  return gulp.src('server/index.js')
+    .pipe(webpackStream(config, webpack))
+    .pipe(gulp.dest('dist/server'));
+});
+
+// Install node_modules for server artifact
+gulp.task('server-node-modules', ['server'],
+  shell.task('yarn install', {
+    cwd: 'dist/server',
+  }),
+);
+
+// AWS Lambda Deployment Package
+gulp.task('lambda', ['server', 'server-node-modules'], (callback) => {
+  const sourcePath = path.resolve(__dirname, 'dist/server');
+  const outputPath = path.resolve(__dirname, 'dist/lambda.zip');
+
+  const file = fs.createWriteStream(outputPath);
+  const archive = archiver('zip', {
+    zlib: { level: 9 },
+  });
+
+  file.on('close', callback);
+  archive.on('progress', (p) => {
+    const percent = Math.round((p.fs.processedBytes / p.fs.totalBytes) * 100);
+    setProgress('lambda', percent, 'compressing');
+  });
+  archive.pipe(file);
+  archive.directory(sourcePath, false);
+  archive.finalize();
+});
+
+gulp.task('default', ['client', 'server', 'lambda']);
