ed0ba81a8b06b5ef8771141c7023b7960ce31ac6
Zhaofeng Li <hello@zhaofeng.li>
2018-04-03 00:15:52 +0000

    credentials.example.js: Add private.mailTransport.from

--- config/credentials.example.js
+++ config/credentials.example.js
@@ -29,6 +29,7 @@ module.exports = {
   //
   // Get yours from https://ethereal.email/create
   mailTransport: {
+    from: 'Kunvet Notifications <notifications@kunvet.com>',
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
