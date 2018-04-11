c1265d9a933bbd0f9073fe95d5d5697563861f14
Zhaofeng Li <hello@zhaofeng.li>
2018-04-11 02:44:59 +0000

    axios.js: Delete debug console calls

--- client/options/axios.js
+++ client/options/axios.js
@@ -1,8 +1,5 @@
 import Config from 'config';
 import Axios from 'axios';
 
-console.log(Config);
-console.log(Config.get('serverUrl'));
-
 Axios.defaults.withCredentials = true;
 Axios.defaults.baseURL = Config.get('serverUrl');
