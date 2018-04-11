9bde072d0eed00ec582bd843c34359ac4f79ddbc
Zhaofeng Li <hello@zhaofeng.li>
2018-04-11 03:04:40 +0000

    development.js: Use the local server

--- config/development.js
+++ config/development.js
@@ -7,7 +7,6 @@
 
 const base = Object.assign({}, require('./production.js'));
 
-// base.serverUrl = 'https://hrmy3ed4n1.execute-api.us-east-1.amazonaws.com/production';
 base.serverUrl = 'http://localhost:3000';
 base.private.database = 'mongodb://localhost/kunvet-dev';
 base.private.files.defaultStorage = 'local';
