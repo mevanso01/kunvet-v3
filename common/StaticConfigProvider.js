da51e6c21f6c9ba7a06ff0d9c612fd27035bc14b
Zhaofeng Li <hello@zhaofeng.li>
2018-04-11 03:48:08 +0000

    StaticConfigProvider.js: Allow the server to override config via env

--- common/StaticConfigProvider.js
+++ common/StaticConfigProvider.js
@@ -3,11 +3,21 @@ import staticConfig from 'static_config';
 import has from 'lodash/has';
 import get from 'lodash/get';
 
+let config = Object.assign({}, staticConfig);
+
+if (typeof process !== 'undefined') {
+  // Server
+  if (process.env.KUNVET_CONFIG) {
+    config = JSON.parse(process.env.KUNVET_CONFIG);
+    console.log('Config overridden by environment variable');
+  }
+}
+
 export default {
   has(key) {
-    return has(staticConfig, key);
+    return has(config, key);
   },
   get(key) {
-    return get(staticConfig, key);
+    return get(config, key);
   },
 };
