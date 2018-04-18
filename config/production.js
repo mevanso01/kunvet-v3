82209d4663b2f296aea308c1187e463a7412b3aa
Zhaofeng Li <hello@zhaofeng.li>
2018-04-18 02:52:10 +0000

    production.js: Switch to https://s.kunvet.com endpoint

--- config/production.js
+++ config/production.js
@@ -41,8 +41,7 @@ if (Credentials.REMOVEMEPLS) {
 }
 
 module.exports = {
-  serverUrl: 'https://hrmy3ed4n1.execute-api.us-east-1.amazonaws.com/production',
-  // serverUrl: 'http://localhost:3000',
+  serverUrl: 'https://s.kunvet.com',
   googleMapsKey: Credentials.googleMapsKey,
   sentry: {
     dsn: Credentials.sentryClientDsn,
