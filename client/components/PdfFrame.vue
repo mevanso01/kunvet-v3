06e5e3115088727f4d9ee73fea2933b4b0fed4db
Zhaofeng Li <hello@zhaofeng.li>
2018-04-20 03:44:50 +0000

    Add PdfFrame

---
+++ client/components/PdfFrame.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div v-for="n in pages">
+      <canvas :id="prefix + '-page-' + n">
+      </canvas>
+    </div>
+  </div>
+</template>
+<script>
+import PdfJs from 'pdfjs-dist';
+import uuidv1 from 'uuid/v1';
+import mutexify from 'mutexify';
+import debounce from 'lodash/debounce';
+
+export default {
+  props: ['href'],
+  data() {
+    return {
+      prefix: 'pdf',
+      pdf: null,
+      pages: 0,
+      containerWidth: 0,
+      renderLock: mutexify(),
+      debouncedResize: debounce(this.onResize, 500),
+    };
+  },
+  created() {
+    this.prefix = uuidv1();
+    window.addEventListener('resize', this.debouncedResize);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.debouncedResize);
+  },
+  mounted() {
+    this.onResize();
+    this.initialLoad();
+  },
+  watch: {
+    href() {
+      this.pages = 0;
+      this.load();
+    },
+  },
+  methods: {
+    onResize() {
+      this.containerWidth = this.$el.clientWidth;
+      this.render();
+    },
+    async initialLoad() {
+      await this.load();
+      this.render();
+    },
+    async load() {
+      this.pdf = await PdfJs.getDocument(this.href);
+      this.pages = this.pdf.numPages;
+      this.$emit('loaded', this.pdf);
+    },
+    render() {
+      this.renderLock(async (release) => {
+        for (let i = 1; i <= this.pages; ++i) {
+          const pageId = `${this.prefix}-page-${i}`;
+          const pageCanvas = document.getElementById(pageId);
+          await this.renderPage(i, pageCanvas);
+        }
+        release();
+      });
+    },
+    async renderPage(n, canvas) {
+      const context = canvas.getContext('2d');
+
+      if (!this.pdf) {
+        context.fillText(0, 0, 'Loading');
+        return;
+      }
+
+      const page = await this.pdf.getPage(n);
+      const pageWidth = page.pageInfo.view[2];
+      const scale = this.containerWidth / pageWidth;
+      const viewport = page.getViewport(scale);
+
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      const renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      };
+      await page.render(renderContext).promise;
+    },
+  },
+};
+</script>
--- client/pages/Sandbox.vue
+++ client/pages/Sandbox.vue
@@ -4,30 +4,21 @@
     <div>
       Play with me on <code>client/pages/Sandbox.vue</code>!
     </div>
-    <FileUploader
-      @created="onCreated"
-      @uploaded="onUploaded"
-    />
+    <PdfFrame href="https://s3.amazonaws.com/kunvet-prod-assets/HW1.pdf" page="1">
+    </PdfFrame>
   </v-container>
 </template>
 <script>
-import FileUploader from '@/components/FileUploader';
+import PdfFrame from '@/components/PdfFrame';
 
 export default {
   components: {
-    FileUploader,
+    PdfFrame,
   },
   data() {
     return {
+      text: 'Default text',
     };
-  },
-  methods: {
-    onCreated(id) {
-      console.log(`✨ File slot created: ${id}`);
-    },
-    onUploaded(id) {
-      console.log(`☁ File uploaded: ${id}`);
-    },
   },
 };
 </script>
