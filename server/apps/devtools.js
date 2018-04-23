edca4e3d1e6ab5a21fe04025127cd8d142617062
Zhaofeng Li <hello@zhaofeng.li>
2018-04-23 00:19:17 +0000

    devtools.js: Add preview_email

--- server/apps/devtools.js
+++ server/apps/devtools.js
@@ -5,6 +5,9 @@ import KoaRouter from 'koa-router';
 // GraphiQL
 import { graphiqlKoa } from 'apollo-server-koa';
 
+// Mailer
+import Mailer from '@/utils/Mailer';
+
 // Scheduler
 import Scheduler from '@/Scheduler';
 
@@ -31,6 +34,7 @@ router.get('/', (ctx) => {
             <li><a href="/login">Log in</li>
             <li><a href="/graphiql">Graph<em>i</em>QL</a></li>
             <li><a href="/trigger_scheduler">Trigger scheduler</li>
+            <li><a href="/preview_email">Preview email</li>
           </ul>
         </p>
       </body>
@@ -58,6 +62,35 @@ router.get('/login', (ctx) => {
       <input name='email' placeholder='E-mail'>
       <input name='password' placeholder='Password'>
       <button type='submit'>Log in</button>
+    </form>
+  </body>
+</html>
+  `;
+});
+
+// Email preview
+router.get('/preview_email', async (ctx) => {
+  if (ctx.query.template) {
+    const mailer = new Mailer();
+    const template = ctx.request.query.template;
+    const locals = JSON.parse(ctx.request.query.locals);
+    ctx.body = await mailer.render(template + '/html', locals);
+    return;
+  }
+  ctx.body = `
+<!doctype html>
+<html>
+  <head>
+    <title>Email preview</title>
+  </head>
+  <body>
+    <form method='get'>
+      <input name='template' placeholder='Template' value='email-verification'><br/>
+      <textarea rows='10' cols='50' name='locals'>{
+  "code": "123abc"
+}
+      </textarea></br/>
+      <button type='submit'>Preview</button>
     </form>
   </body>
 </html>
