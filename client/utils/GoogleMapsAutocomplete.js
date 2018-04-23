4e3d5dbede02b90a84c8d9abca765166d6321b07
Zhaofeng Li <hello@zhaofeng.li>
2018-04-23 01:23:15 +0000

    client/utils: Add GoogleMapsAutocomplete.js

---
+++ client/utils/GoogleMapsAutocomplete.js
@@ -0,0 +1,42 @@
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+function getInputElement(el) {
+  let domel = typeof el.$el !== 'undefined' ? el.$el : el;
+
+  if (domel.tagName.toLowerCase() !== 'input') {
+    const inputs = domel.getElementsByTagName('input');
+    if (!inputs.length) {
+      throw Error(`No input fields found for ${el}`);
+    }
+
+    domel = inputs[0];
+  }
+
+  return domel;
+}
+
+export default {
+  async attach(el, onchange = () => {}) {
+    console.log(el);
+    if (!window.google || !window.google.maps) {
+      await VueGoogleMaps.loaded;
+    }
+
+    const domel = getInputElement(el);
+    domel.setAttribute('placeholder', '');
+
+    if (!domel.googleMapsAutocomplete) {
+      domel.googleMapsAutocomplete = new window.google.maps.places.Autocomplete(domel);
+      domel.googleMapsAutocomplete.addListener('place_changed', () => {
+        onchange(domel.googleMapsAutocomplete.getPlace());
+      });
+    }
+  },
+  detach(el) {
+    const domel = getInputElement(el);
+
+    if (domel.googleMapsAutocomplete) {
+      domel.googleMapsAutocomplete.unbindAll();
+    }
+  },
+};
