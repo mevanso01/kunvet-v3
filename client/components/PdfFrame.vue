5c835320af5e494802697fab1178df7f967889b3
Zhaofeng Li <hello@zhaofeng.li>
2018-04-29 22:04:33 +0000

    PdfFrame.vue: Automatically clip the PDF

--- client/components/PdfFrame.vue
+++ client/components/PdfFrame.vue
@@ -1,9 +1,14 @@
 <template>
-  <div :id="prefix + '-pdfframe'">
+  <div :id="prefix + '-pdfframe'" :style="frameStyle" class="frame">
     <div v-for="n in pages">
       <canvas :id="prefix + '-page-' + n" class="page">
       </canvas>
     </div>
+    <transition name="slide-y-transition">
+      <div class="more" v-if="clipped" @click="clipped = false">
+        <span>View More</span>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -20,6 +25,10 @@ export default {
       pdf: null,
       pages: 0,
       containerWidth: 0,
+      pageLimit: 3,
+      clipped: false,
+      pageHeights: [],
+      frameStyle: {},
       renderLock: mutexify(),
       debouncedResize: debounce(this.onResize, 500),
     };
@@ -41,6 +50,9 @@ export default {
     href() {
       this.pages = 0;
       this.initialLoad();
+    },
+    clipped() {
+      this.updateClipping();
     },
   },
   methods: {
@@ -72,16 +84,20 @@ export default {
       }
 
       this.pages = this.pdf.numPages;
+      this.clipped = this.pages > this.pageLimit;
       this.$emit('loaded', this.pdf);
     },
     render() {
       window.setImmediate(() => {
         this.renderLock(async (release) => {
+          this.pageHeights = [];
+
           for (let i = 1; i <= this.pages; ++i) {
             const pageId = `${this.prefix}-page-${i}`;
             const pageCanvas = document.getElementById(pageId);
             await this.renderPage(i, pageCanvas);
           }
+
           release();
         });
       });
@@ -102,17 +118,55 @@ export default {
       canvas.height = viewport.height;
       canvas.width = viewport.width;
 
+      this.pageHeights[n - 1] = viewport.height;
+      this.updateClipping();
+
       const renderContext = {
         canvasContext: context,
         viewport: viewport,
       };
       await page.render(renderContext).promise;
     },
+    updateClipping() {
+      if (!this.clipped || !this.pageHeights.length) {
+        this.frameStyle = {};
+        return;
+      }
+
+      const height = this.pageHeights.slice(0, this.pageLimit).reduce(
+        (a, c) => a + c,
+      );
+
+      this.frameStyle = {
+        height: `${height}px`,
+        'overflow-y': 'hidden',
+      };
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
+.frame {
+  transition: height .2s;
+}
 .page {
   pointer-events: none;
+}
+.more {
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100px;
+  font-size: 20px;
+  background: linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, .80) 60%, rgba(255, 255, 255, 0));
+  cursor: pointer;
+
+  span {
+    margin: 0 auto;
+    align-self: flex-end;
+  }
 }
 </style>
