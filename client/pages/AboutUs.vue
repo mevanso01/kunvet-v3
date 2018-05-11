d7370adaa33f118f0300bdde58a56f7443a998f5
badboybigbill <bz@kunvet.com>
2018-05-11 00:58:10 -0700

    Update AboutUs.Vue
    Minor grammar fixes
    Added another section

--- client/pages/AboutUs.vue
+++ client/pages/AboutUs.vue
@@ -9,17 +9,20 @@
                 <p>Kunvet is a job platform that's designed for students. Our focus is to help students, including international students, to find jobs or opportunities immediately nearby.</p>
 
                 <h2 style="color: #969494;">Why use kunvet.com instead?</h2>
-                <p>We will provide the most recent and relevant opportunities for our students free of charge.<br>We will include as many unique opportunities, which you cannot find on any other platforms.</p>
+                <p>We will provide the most recent and relevant opportunities for our students free of charge. Try to beat that.<br>We will include many unique opportunities, which you cannot find on any other platforms.</p>
 
                 <h2 style="color: #969494;">Who are we?</h2>
-                <p>We are a group of entrepreneurial students who are sick and tired of the existing job platforms, flyers all around campus, and having to walk around the city to find jobs nearby!</p>
+                <p>We are a group of entrepreneurial students who are sick and tired of the existing job platforms, flyers all around on-campus, and having to walk around the city to find jobs nearby!</p>
 
                 <h2 style="color: #969494;">What have we done?</h2>
-                <p>In the previous version of Kunvet, we had 3000+ unique visits from college students, posted more than 200 jobs on our website and more than 80 registered students.<br>These demands are the reason we created the current version of Kunvet.</p>
+                <p>In the previous version of Kunvet, we had 3000+ unique visits from college students, posted more than 200 jobs and more than 80 registered students.<br>These demands are the reason we created the current version of Kunvet.</p>
 
                 <h2 style="color: #969494;">What next?</h2>
                 <p>Many opportunities listed elsewhere are not student-friendly or unique. Instead, many of existing online jobs are repetitive and redundant.<br>So, our next step is to invite organizations, clubs and individuals from the local community to post student-friendly, unique and nearby jobs and opportunities on Kunvet.</p>
 
+                <h2 style="color: #969494;">How you can help Kunvet?</h2>
+                <p>Honestly, we don't think we're gonna set up a donation page and ask for your money, which probably you don't even have enough to spend as a student yourself. Ouch.<br>If you like Kunvet, just tell your friends about us! Tell them that there's this awesome job platform for students!</p>
+                
                 <h2 style="color: #969494;">We invite you to join us</h2>
                 <p>If you have experience with web development or marketing, we invite you to <router-link to="/join"><b><u>join our start-up group</u></b></router-link>.</p>
 
