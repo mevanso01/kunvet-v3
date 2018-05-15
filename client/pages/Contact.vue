092b2a6d1ed04209c00ebe5162b4ef62a86188ff
badboybigbill <bz@kunvet.com>
2018-05-14 23:23:59 -0700

    Create Contact.vue

---
+++ client/pages/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+  <div>
+        <div class="main-cont-large">
+            <div style="padding: 20px;">
+                <h1 style="margin-bottom: 20px;">Contact us</h1>
+             
+                <p>If you have any questions about Kunvet, please don't hesitate to reach us at <a href="mailto:bz@kunvet.com">bz@kunvet.com</a> or call us at 949-861-1085</p>
+            </div>
+        </div>
+    </div>
+  </v-container>
+</template>
+<script>
+</script>
