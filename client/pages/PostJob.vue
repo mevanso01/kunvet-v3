0dbc20fa60feef83abcdba9d3c4890b1bf7b7689
badboybigbill <bz@kunvet.com>
2018-05-16 16:24:21 -0700

    Update PostJob.vue

--- client/pages/PostJob.vue
+++ client/pages/PostJob.vue
@@ -15,10 +15,11 @@
             </p>
 
             <p><router-link to="/signup"><b><u>I like it, take me to sign up!</u></b></router-link></p>
-            <v-btn class="kunvet-red-bg" dark @click=router.push("signup")>I like it, take me to sign up!</v-btn>
+            <v-btn class="kunvet-red-bg" dark @click="router.push('signup')">I like it, take me to sign up!</v-btn>
         </div>
     </div>
   </v-container>
 </template>
 <script>
+
 </script>
