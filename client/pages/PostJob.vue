105aec17b76f3bd92a98049d10078442ea814dcd
badboybigbill <bz@kunvet.com>
2018-05-16 15:55:24 -0700

    Update PostJob.vue

--- client/pages/PostJob.vue
+++ client/pages/PostJob.vue
@@ -4,15 +4,18 @@
         <div style="padding: 20px;">
             <h1>We're glad you're here</h1>
 
-            <p>Kunvet is designed for quicker hiring for smaller organization and personal hiring. Kunvet acts as a pre-screening process for your hiring.</p>
-            <p>Time is valuable to students and employers, so we ask employers to make up their decision within 35 days before actions are restricted to an application.</p>
+            <p style="font-size: 1.3em;">Kunvet is designed for quicker hiring for smaller organization and personal hiring. Kunvet acts as a pre-screening process for your hiring.<br>
+            Time is valuable to students and employers, so we ask employers to make up their decision within 35 days before actions are restricted to an application.
+            </p>
 
             <h1>How it works</h1>
-            <p>Depending on your need, begin by choosing the appropriate type of account.</p>
-            <p>After students apply, their information will be organized under the Applicants page. You may <b>Accept</b> or <b>Reject</b> her, whom we’ll notify for you.</p>
-            <p>If you found a good candidate, you may email her and continue your usual hiring process, after clicking on <b>Accept</b> or <b>Reject</b>.</p>
+            <p style="font-size: 1.3em;">Depending on your need, begin by choosing the appropriate type of account.<br>
+            After students apply, their information will be organized under the Applicants page. You may <b>Accept</b> or <b>Reject</b> her, whom we’ll notify for you.<br>
+            If you found a good candidate, you may email her and continue your usual hiring process, after clicking on <b>Accept</b> or <b>Reject</b>.
+            </p>
 
             <p><router-link to="/signup"><b><u>I like it, take me to sign up!</u></b></router-link></p>
+            <v-btn class="kunvet-red-bg" dark><router-link to="/signup">I like it, take me to sign up!</router-link></v-btn>
         </div>
     </div>
   </v-container>
