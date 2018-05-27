ce15b4e6bf659267f4336601d7dbee594bd40b3d
Zhaofeng Li <hello@zhaofeng.li>
2018-05-27 06:00:55 +0000

    Validate.vue: Redirect user to account page after validation

--- client/pages/Validate.vue
+++ client/pages/Validate.vue
@@ -8,24 +8,33 @@
   <v-container fluid style="padding-left: 0; padding-right: 0;">
     <div style="padding: 60px 0;">
       <p v-if="loading" class="center val-c-p">Validating. This should take just a few seconds.</p>
-      <div v-if="!loading && !isvalid" class="center val-c-p">
-        <p>
-          Validation unsuccessful, please request a new
-          validation email on <a href="/account">your profile page</a>.
-        </p>
-        <p>
-          It's also possible that you have already verified
-          your email. In that case, you are good to go!
-        </p>
-      </div>
-      <div v-if="!loading && isvalid" style="max-width: 600px; margin: auto;">
-        <v-card style="margin: 15px;">
-          <v-card-text>
-              You're all set! Now you can log in with the email and password
-              you provided, and finish creating your account
-          </v-card-text>
-        </v-card>
-      </div>
+      <template v-else>
+        <div v-if="!isvalid" class="center val-c-p">
+          <p>
+            Validation unsuccessful, please request a new
+            validation email on <a href="/account">your profile page</a>.
+          </p>
+          <p>
+            It's also possible that you have already verified
+            your email. In that case, you are good to go!
+          </p>
+        </div>
+        <div v-else style="max-width: 600px; margin: auto;">
+          <v-card style="margin: 15px;">
+            <v-card-text>
+              You're all set!
+              <template v-if="redirecting">
+                We are taking you to the account page where you can finish
+                creating your account.
+              </template>
+              <template v-else>
+                Now you can log in with the email and password you provided,
+                and finish creating your account.
+              </template>
+            </v-card-text>
+          </v-card>
+        </div>
+      </template>
     </div>
   </v-container>
 </template>
@@ -38,6 +47,7 @@ export default {
     return {
       loading: true,
       isvalid: false,
+      redirecting: false,
       dne: null,
     };
   },
@@ -48,13 +58,22 @@ export default {
   methods: {
     validateCode() {
       axios.post('/auth/verify', { code: this.code }).then((res) => {
-        this.loading = false;
-        if (res.data.success) {
-          this.isvalid = true;
-        }
+        this.completeValidation(res.data.success);
       }).catch(() => {
         this.loading = false;
       });
+    },
+    completeValidation(success) {
+      this.loading = false;
+      this.isvalid = success;
+
+      if (this.isvalid && this.$store.state.userID) {
+        // Redirect to Account page
+        this.redirecting = true;
+        setTimeout(() => {
+          this.$router.push('/account');
+        }, 1000);
+      }
     },
   },
 };
