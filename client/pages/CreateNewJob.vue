14ccde08db994947f2ae22764d0bc2e3b5fe5c97
Leo Vinogradov <leovinogradov@gmail.com>
2018-05-30 00:03:48 -0700

    commented out my changes to createnewjob, for now

--- client/pages/CreateNewJob.vue
+++ client/pages/CreateNewJob.vue
@@ -1189,12 +1189,9 @@ export default {
     if (this.$store.state.acct === 2 && this.$store.state.bdata) {
       this.posted_by = this.$store.state.bdata.business_name;
       // Autofill address
-      if (this.$store.state.bdata.address) {
+      /* if (this.$store.state.bdata.address && !this.$route.params.id) {
         this.address = this.$store.state.bdata.address;
-        const p = this.autocomplete.getPlace();
-        console.log('P', p);
-        this.setPlace(p);
-      }
+      } */
       this.uid = this.$store.state.userID;
       if (this.$route.params.id) {
         this.getEditJobData(this.$route.params.id);
@@ -1237,6 +1234,10 @@ export default {
       this.autocomplete.addListener('place_changed', () => {
         this.setPlace(this.autocomplete.getPlace());
       });
+      /* if (this.address) {
+        const p = this.autocomplete.getPlace();
+        console.log('P', p);
+      } */
     });
   },
 };
