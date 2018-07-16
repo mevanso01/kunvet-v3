ea596f7ff3aa518e43dd15add41cf93938f4e393
badboybigbill <bz@kunvet.com>
2018-07-16 11:52:05 -0700

    Update Location.js
    Some colleges turned out to be only institutions, due to my previous insufficient research.
    Shortened "University of California, xxx" to "UC xxx".
    Removed "UCLA" & "UCR" for now as some employers thought that we already service those areas and thus posted jobs in those areas. Those jobs didn't receive any applicants, which may made them think nobody use Kunvet. However, that's not true, so I temporarily deleted UCLA & UCR.

--- client/constants/locations.js
+++ client/constants/locations.js
@@ -1,16 +1,14 @@
 export default {
   search_locations: [
-    { name: 'University of California, Irvine', latitude: 33.6460516, longitude: -117.8427444 },
+    { name: 'UC Irvine', latitude: 33.6460516, longitude: -117.8427444 },
     { name: 'Irvine Valley College', latitude: 33.6754033, longitude: -117.77773 },
     { name: 'Concordia University Irvine', latitude: 33.6536915, longitude: -117.8108672 },
     { name: 'Brandman University', latitude: 33.656335, longitude: -117.769041 },
     { name: 'Stanbridge University', latitude: 33.6788293, longitude: -117.857653 },
-    { name: 'West Coast University', latitude: 33.645193, longitude: -117.857612 },
-    { name: 'American Career College', latitude: 34.079320, longitude: -118.292474 },
-    { name: 'Western State College of Law at Argosy University', latitude: 33.658210, longitude: -117.761329 },
+    { name: 'FIDM Orange County', latitude: 33.691959, longitude: -117.852163 },
+    { name: 'CSFU Irvine', latitude: 33.658768, longitude: -117.7626014 },
+    { name: 'Orange Coast College', latitude: 33.6711302, longitude: -117.9123943 },
     { name: 'California Southern University', latitude: 33.692040, longitude: -117.918428 },
-    { name: 'Fashion Institute of Design & Merchandising, Orange County', latitude: 33.691959, longitude: -117.852163 },
-    { name: 'University of California, Los Angeles', latitude: 34.068921, longitude: -118.4451811 },
-    { name: 'University of California, Riverside', latitude: 33.9737055, longitude: -117.3280644 },
+    { name: 'Coastline Community College', latitude: 33.7156043, longitude: -117.9314271 },
   ],
 };
