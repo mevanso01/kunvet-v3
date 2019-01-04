24ec19e7c49cc704e0eb84d873242bbc2267a78a
Zhaofeng Li <hello@zhaofeng.li>
2019-01-04 10:37:36 -0800

    generate-credentials.js: Separate dev/prod credential variables

--- .circleci/generate-credentials.js
+++ .circleci/generate-credentials.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 
-let credentials = process.env.CREDENTIALS_FILE;
+const prodCredentials = process.env.PROD_CREDENTIALS_FILE;
+const devCredentials = process.env.DEV_CREDENTIALS_FILE;
 const branch = process.env.CIRCLE_BRANCH;
 
-if (!credentials || !branch) {
-  console.log('No CREDENTIALS_FILE or CIRCLE_BRANCH');
+if (!prodcredentials || !devCredentials || !branch) {
+  console.log('No credential environment variables');
   console.log('This build will be broken.');
   fs.writeFileSync('config/credentials.js', '/* CI */ module.exports = {};');
 } else {
-  const serverUrlMap = {
-    master: 'https://ds.kunvet.com',
-    prod: 'https://s.kunvet.com',
-  };
-  const serverUrl = serverUrlMap[branch] || 'https://broken.build.tld';
-  credentials = credentials.replace('%SERVERURL%', serverUrl);
+  credentials = branch === 'prod' ? prodCredentials : devCredentials;
   fs.writeFileSync('config/credentials.js', credentials);
 }
