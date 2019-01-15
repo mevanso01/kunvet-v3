f081bf27d4208eb8eda10163f4f8aad0b5238728
Zhaofeng Li <hello@zhaofeng.li>
2019-01-14 22:16:31 -0800

    Login.vue: Remove unused code

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,20 +1,3 @@
-<style scoped>
-.link{
-    cursor:pointer;
-    text-align:center;
-}
-a:hover{
-    cursor:pointer;
-}
-.login-section {
-  padding: 16px 0;
-}
-@media (min-width: 600px) {
-  .login-section {
-    padding: 16px 32px;
-  }
-}
-</style>
 <template>
 <v-container fluid class="white-bg">
   <LoginComponent @loggedIn="triggerRedirect"></LoginComponent>
@@ -22,56 +5,17 @@ a:hover{
 
 </template>
 <script>
-// import App from '@/App';
-import Axios from 'axios';
-// import KunvetError from '#/KunvetError';
-// import EventBus from '@/EventBus';
 import LoginComponent from '@/components/LoginComponent';
 
 export default {
   data() {
     return {
-      e1: true,
-      valid: false,
-      forgetpwd: 0,
-      bad_login: false,
-      loggedIn: false,
-      sent: false,
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /^\w+([-.]?\w+)*@\w+([-.]?\w+)*(\.\w+)+$/.test(v) || 'E-mail must be valid',
-      ],
-      password: '',
-      passwordRules: [
-        v => !!v || 'Required',
-      ],
-      loading: false,
     };
   },
   components: {
     LoginComponent,
   },
   methods: {
-    requestPasswordReset() {
-      const valid = this.$refs.form2.validate();
-      if (!this.loading && valid) {
-        this.loading = true;
-        Axios.post('/reset-password/request-reset', { email: this.email }).then((res) => {
-          this.loading = false;
-          if (res.data.success) {
-            this.sent = true;
-          } else {
-            // not successful
-            this.sent = true; // mark as true anyway
-          }
-        }).catch((error) => {
-          // Network error
-          this.loading = false;
-          this.$error(error);
-        });
-      }
-    },
     triggerRedirect() {
       let target = this.$route.query.redirect;
       // check if target is a restricted redirect page
