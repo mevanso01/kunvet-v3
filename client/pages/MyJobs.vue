80f5833b65e5f489fdd1ac258e6931b611e58a7e
vkrishn2 <vkrishn2@uci.edu>
2019-03-16 21:51:42 -0700

    myJobs alignment issue fixed

--- client/pages/MyJobs.vue
+++ client/pages/MyJobs.vue
@@ -85,14 +85,18 @@
             <v-flex xs12 sm6 md5>
               <v-layout>
                 <v-flex xs7 sm7  style="padding: 0px">
-                  <p style="padding-top: 50px; padding-right: 15px;">You haven't posted the job yet,   <br>continue working on it!  </p>
+                  <p>Applicants</p>
+                  <h2 style="color: #448ef6">-</h2>
+                  <k-btn color="#448ef6" small disabled>
+                    View Applicants
+                  </k-btn>
                 </v-flex>
-                <v-flex xs5 sm5  style="padding: 0px text-align: center">
+                <v-flex xs5 sm5  style="padding: 0px">
                   <p>Status</p>
                   <h2 style="color: orange">Unposted</h2>
                   <router-link :to="`/createjob/${job._id}`">
                     <k-btn color="orange" small>
-                      Post It
+                      Post It 
                     </k-btn>
                   </router-link>
                 </v-flex>
