305a743db616f08c869ea24d8c8f37714ef3594d
Zhaofeng Li <hello@zhaofeng.li>
2019-04-06 19:55:44 -0700

    Scheduler.js: Add job expiration check

--- server/Scheduler.js
+++ server/Scheduler.js
@@ -12,8 +12,33 @@
  */
 import Config from 'config';
 import Logger from 'winston';
+import Models from '@/mongodb/Models';
 
-const tasks = [];
+const tasks = [
+  () => { // filter all expired jobs and update attribute
+    Models.Job
+      .find({}, (err, docs) => {
+        const toExpire = docs // all jobs whose expiration date have passed
+          .filter(job => {
+            const expiration = job.expiry_date;
+            const today = new Date();
+            return today.getTime() > expiration.getTime();
+          })
+          .map(job => job._id); // extract id from job object
+        console.log(toExpire);
+        for (let i = 0; i < toExpire.length; ++i) {
+          Models.Job.findOneAndUpdate(
+            { '_id': toExpire[i]._id },
+            { $set: { 'expired': true } },
+            { new: true },
+            (err1, docs1) => {
+              console.log(docs1);
+            },
+          );
+        }
+      });
+  },
+];
 
 export default class Scheduler {
   static install() {
