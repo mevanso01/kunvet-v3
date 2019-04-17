f97870954a8044f84c73849f388fc0c0996c86fe
badboybigbill <bz@kunvet.com>
2019-04-16 23:43:51 -0700

    Update About Us Styling
    grammar needs review

--- client/pages/AboutUs.vue
+++ client/pages/AboutUs.vue
@@ -1,27 +1,32 @@
 <template>
-  <v-container fluid>
-  <div>
-        <div class="main-cont-large">
-            <div style="padding: 20px;">
-                <h1 style="margin-bottom: 20px;">About us</h1>
+    <v-container fluid>
+        <div>
+            <div class="main-cont-large">
+                <h1 style="font-family:'Lora', serif; font-weight:400; font-size:3.2em; margin:40px 0; display:inline-block; line-height:90%;
+                background:linear-gradient(to right, #FF5F6D 0%, #FFC371 100%) left bottom #fff no-repeat; padding-bottom:4px; background-size:100% 12px;">About Us</h1>
 
-                <h2 style="color: #969494;">What is Kunvet?</h2>
-                <p>Kunvet is a professional and local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for someone nearby to collaborate with them on their project to hiring a new employee locally.</p>
+                <div style="margin-bottom:30px;">
+                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is Kunvet?</h2>
+                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">Kunvet is a professional, local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for help on a personal project to hiring a new employee.</p>
+                </div>
 
-                <h2 style="color: #969494;">Why choose Kunvet?</h2>
-                <p>Kunvet is user-centric. We put the needs of our users as our top priority.<br><br>
-                For an opportunity-seeker, Kunvet provides her with the latest opportunities in the local community. On top of that, she has much more control over her application sent through Kunvet, since she would know what happened to her application after she has applied to an opportunity.<br><br>
-                For someone seeking for help, Kunvet provides him with local and relevant applicants within a short amount of time. On top of that, it costs as little as $5 to seek for help on Kunvet.</p>
+                <div style="margin-bottom:30px;">
+                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">Why choose Kunvet?</h2>
+                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">Kunvet is user-centric. We put the needs of our users as our top priority.<br>
+                  For an opportunity-seeker, Kunvet provides her with the latest opportunities in the local community. On top of that, she has much more control over her application sent through Kunvet, since she would know what happened to her application after she has applied to an opportunity.<br>
+                  For someone seeking for help, Kunvet provides him with local and relevant applicants within a short amount of time. On top of that, it costs as little as $5 to seek for help on Kunvet.</p>
+                </div>
 
-                <h2 style="color: #969494;">What is our Mission?</h2>
-                <p>We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
+                <div style="margin-bottom:30px;">
+                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is our Mission?</h2>
+                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
+                </div>
 
-                <h2 style="color: #969494;">What is next?</h2>
-                <p>We are constantly upgrading our platform to provide our users with better service.</p>
+                <div style="margin-bottom:30px;">
+                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is next?</h2>
+                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">We are constantly upgrading our platform to provide our users with better service.</p>
+                </div>
             </div>
         </div>
-    </div>
-  </v-container>
+    </v-container>
 </template>
-<script>
-</script>
