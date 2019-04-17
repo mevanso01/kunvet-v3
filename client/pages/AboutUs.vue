c6eb9bb77e82a8a4ee1a6717850a8be8f379214b
badboybigbill <bz@kunvet.com>
2019-04-17 00:06:34 -0700

    Update About Us CSS

--- client/pages/AboutUs.vue
+++ client/pages/AboutUs.vue
@@ -6,28 +6,28 @@
     <v-container fluid>
         <div>
             <div class="main-cont-large">
-                <h1 style="">About Us</h1>
+                <h1>About Us</h1>
 
-                <div style="margin-bottom:30px;">
-                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is Kunvet?</h2>
-                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">Kunvet is a professional, local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for help on a personal project to hiring a new employee.</p>
+                <div class="section">
+                  <h2>What is Kunvet?</h2>
+                  <p>Kunvet is a professional, local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for help on a personal project to hiring a new employee.</p>
                 </div>
 
-                <div style="margin-bottom:30px;">
-                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">Why choose Kunvet?</h2>
-                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">Kunvet is user-centric. We put the needs of our users as our top priority.<br>
+                <div class="section">
+                  <h2>Why choose Kunvet?</h2>
+                  <p>Kunvet is user-centric. We put the needs of our users as our top priority.<br>
                   For an opportunity-seeker, Kunvet provides her with the latest opportunities in the local community. On top of that, she has much more control over her application sent through Kunvet, since she would know what happened to her application after she has applied to an opportunity.<br>
                   For someone seeking for help, Kunvet provides him with local and relevant applicants within a short amount of time. On top of that, it costs as little as $5 to seek for help on Kunvet.</p>
                 </div>
 
-                <div style="margin-bottom:30px;">
-                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is our Mission?</h2>
-                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
+                <div class="section">
+                  <h2>What is our Mission?</h2>
+                  <p>We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
                 </div>
 
-                <div style="margin-bottom:30px;">
-                  <h2 style="font-family:'Lora', serif; font-weight:400; font-size:1.4em; margin-top:0; margin-bottom:10px;">What is next?</h2>
-                  <p style="font-family:'Open Sans', sans-serif; font-size:1em; margin:0; line-height:180%;">We are constantly upgrading our platform to provide our users with better service.</p>
+                <div class="section">
+                  <h2>What is next?</h2>
+                  <p>We are constantly upgrading our platform to provide our users with better service.</p>
                 </div>
             </div>
         </div>
