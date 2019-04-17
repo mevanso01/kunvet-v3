c82919de6e2bbb7cd873b2cede03267701ffbd31
badboybigbill <bz@kunvet.com>
2019-04-17 00:28:04 -0700

    Updated About Us CSS

--- client/pages/AboutUs.vue
+++ client/pages/AboutUs.vue
@@ -4,30 +4,30 @@
 
 <template>
     <v-container fluid>
-        <div>
-            <div class="main-cont-large">
+        <div class="main-cont-large">
+            <div class="about">
                 <h1>About Us</h1>
 
                 <div class="section">
-                  <h2>What is Kunvet?</h2>
-                  <p>Kunvet is a professional, local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for help on a personal project to hiring a new employee.</p>
+                    <h2>What is Kunvet?</h2>
+                    <p>Kunvet is a professional, local collaboration platform, where people looking for help are connected with people seeking for new opportunities. People use Kunvet for a variety of purposes, from looking for help on a personal project to hiring a new employee.</p>
                 </div>
 
                 <div class="section">
-                  <h2>Why choose Kunvet?</h2>
-                  <p>Kunvet is user-centric. We put the needs of our users as our top priority.<br>
-                  For an opportunity-seeker, Kunvet provides her with the latest opportunities in the local community. On top of that, she has much more control over her application sent through Kunvet, since she would know what happened to her application after she has applied to an opportunity.<br>
-                  For someone seeking for help, Kunvet provides him with local and relevant applicants within a short amount of time. On top of that, it costs as little as $5 to seek for help on Kunvet.</p>
+                    <h2>Why choose Kunvet?</h2>
+                    <p>Kunvet is user-centric. We put the needs of our users as our top priority.<br>
+                    For an opportunity-seeker, Kunvet provides her with the latest opportunities in the local community. On top of that, she has much more control over her application sent through Kunvet, since she would know what happened to her application after she has applied to an opportunity.<br>
+                    For someone seeking for help, Kunvet provides him with local and relevant applicants within a short amount of time. On top of that, it costs as little as $5 to seek for help on Kunvet.</p>
                 </div>
 
                 <div class="section">
-                  <h2>What is our Mission?</h2>
-                  <p>We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
+                    <h2>What is our Mission?</h2>
+                    <p>We once were people searching for new opportunities, too. We were frustrated of seeing irrelevant, repetitive and insufficient amount of local opportunities on the internet. So, we decided to create a platform where people would not experience the frustrations we did.</p>
                 </div>
 
                 <div class="section">
-                  <h2>What is next?</h2>
-                  <p>We are constantly upgrading our platform to provide our users with better service.</p>
+                    <h2>What is next?</h2>
+                    <p>We are constantly upgrading our platform to provide our users with better service.</p>
                 </div>
             </div>
         </div>
