caf9c7dac939b17c1b2515a41ef1086d0bf4f2f8
Zhaofeng Li <hello@zhaofeng.li>
2019-04-20 20:45:14 -0700

    server: Add test code for transaction posting

--- server/apps/billing.js
+++ server/apps/billing.js
@@ -2,6 +2,7 @@
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
 import BraintreeGateway from '@/BraintreeGateway';
+import util from 'util';
 
 const bodyParser = require('koa-bodyparser');
 
@@ -9,6 +10,33 @@ const app = new Koa();
 const router = new KoaRouter();
 
 app.use(bodyParser());
+
+const ACTIONS = {
+  activateJob: {
+    description: 'Post a draft job, or re-post an expired job',
+    price: 500,
+    validate(ctx, action) {
+      console.log(ctx, action);
+      return true;
+    },
+    fulfill(ctx, action) {
+      console.log(ctx, action);
+      return true;
+    },
+  },
+  promoteJob: {
+    description: 'Promote a job',
+    price: 200,
+    validate(ctx, action) {
+      console.log(ctx, action);
+      return true;
+    },
+    fulfill(ctx, action) {
+      console.log(ctx, action);
+      return true;
+    },
+  },
+};
 
 function generateToken(options = {}) {
   return new Promise((resolve, reject) => {
@@ -74,6 +102,78 @@ router.get('/getAuthorization', async (ctx) => {
   });
 
   ctx.body = JSON.stringify(response.clientToken);
+});
+
+router.post('/postTransaction', async (ctx) => {
+  if (!ctx.isAuthenticated() || !ctx.state.user) {
+    ctx.status = 401;
+    ctx.body = JSON.stringify({
+      success: false,
+      message: 'Must be authenticated',
+    });
+    return;
+  }
+
+  const req = ctx.request.body;
+
+  // Validation
+  let credits = 0;
+
+  for (const action of req.actions) {
+    if (!Object.prototype.hasOwnProperty.call(ACTIONS, action.name)) {
+      // Invalid action
+      ctx.status = 401;
+      ctx.body = JSON.stringify({
+        success: false,
+        message: `${action.name} is not a valid action`,
+      });
+      return;
+    }
+    try {
+      ACTIONS[action.name].validate(ctx, action);
+    } catch (e) {
+      ctx.status = 401;
+      ctx.body = JSON.stringify({
+        success: false,
+        message: `${action.name} failed validation: ${e.getMessage()}`,
+      });
+      return;
+    }
+
+    credits += ACTIONS[action.name].price;
+  }
+
+  // Charge
+  try {
+    const sale = util.promisify(BraintreeGateway.transaction.sale).bind(BraintreeGateway.transaction);
+    const result = await sale({
+      amount: (credits / 100).toString(),
+      paymentMethodNonce: req.paymentMethodNonce,
+      options: {
+        submitForSettlement: true,
+      },
+    });
+    console.log(result);
+  } catch (e) {
+    ctx.status = 401;
+    ctx.body = JSON.stringify({
+      success: false,
+      message: `Failed to charge the user: ${e.getMessage()}`,
+    });
+    // return;
+  }
+
+  ctx.body = JSON.stringify({
+    success: true,
+    message: 'Thanks for the cash!',
+  });
+
+  /*
+
+  // Fulfillment
+  for (const action of req.actions) {
+  }
+  */
 });
 
 app.use(router.routes());
