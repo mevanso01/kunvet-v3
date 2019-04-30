565fdd5abf4e65bead088b0558e4b94cba21d676
vkrishn2 <vkrishn2@uci.edu>
2019-04-29 17:22:37 -0700

    sandbox

--- client/pages/Sandbox.vue
+++ client/pages/Sandbox.vue
@@ -1,89 +1,45 @@
+
+<style scoped>
+/*eslint-disable */
+.form{
+  max-width: 480px; 
+  margin-top: 100px;
+  border: 1px solid black;
+}
+.form_header{
+  font-weight: bold;
+  font-size: 27px;
+  padding-left: 40px; 
+  padding-top: 20px;
+}
+.form_headline{
+  font-weight: bold;
+  font-size: 12px;
+  padding-left: 40px;
+  padding-bottom: 10px;
+}
+.form_checkbox{
+  padding-top: 40px;
+}
+.form_checkbox_label{
+  font-size: 12px;
+  text-transform: uppercase;
+  line-height:32px;
+  font-weight: bolder;
+}
+
+.form_subscribe{
+  position: absolute;
+  margin-top: 40px;
+  margin-left: 300px;
+  height: 35px !important;
+  line-height: 35px !important;
+}
+
+</style>
 <template>
-
+<div style="width:600px; height: 400px;">
+  <div class="_form_1"></div>
+</div>
 </template>
-<script>
-/* eslint-disable no-unused-vars */
-import gql from 'graphql-tag';
-import CodeVerification from '@/components/CodeVerification';
-
-import ResumeUploader from '@/components/ResumeUploader';
-import FileClient from '@/utils/FileClient';
-import PdfFrame from '@/components/PdfFrame';
-
-export default {
-  data() {
-    return {
-      mockName: 'John Smith',
-      mockApplicants: [],
-      mockResume: {
-        name: 'Test',
-        filename: '5c0ad86ac9b77d6d0b33ccd5',
-        resumeid: null,
-      },
-      showFileModal: false,
-      pageSize: 10,
-      page: 0,
-      showSideResume: false,
-    };
-  },
-  apollo: {
-    findJobs: {
-      query: gql`
-        query ($limit: Int!, $skip: Int!) {
-          findJobs (limit: $limit, skip: $skip) {
-            _id
-            title
-          }
-        }`,
-      variables: {
-        limit: 10,
-        skip: 0,
-      },
-    },
-  },
-  components: {
-    CodeVerification,
-    PdfFrame,
-    ResumeUploader,
-  },
-  methods: {
-    testFunc() {
-      console.log('testFunc called');
-    },
-    getSrc(resume) {
-      // eslint-disable-next-line
-      console.log('Test', resume);
-      const src = FileClient.getLink(resume.filename);
-      return src;
-    },
-    addApplicant(_filename, _resumename) {
-      this.mockApplicants.push({
-        name: this.mockName,
-        resume: this.mockResume,
-        showResume: false,
-      });
-      // this.mockName = '';
-      // this.mockResume = {};
-    },
-    resumeUploaded(_filename, _resumename) {
-      this.mockResume = {
-        name: _resumename,
-        filename: _filename,
-        resumeid: null,
-      };
-      this.showFileModal = false;
-    },
-    reloadPage() {
-      this.$apollo.queries.findJobs.fetchMore({
-        variables: {
-          limit: this.pageSize,
-          skip: this.pageSize * this.page,
-        },
-        updateQuery(previousResult, { fetchMoreResult }) {
-          return fetchMoreResult;
-        },
-      });
-    },
-  },
-};
-</script>
+<script src="https://kunvet.activehosted.com/f/embed.php?id=1" type="text/javascript" charset="utf-8"></script>
