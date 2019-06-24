843fc827a586979c86986e0df0173376ba00b904
Zhaofeng Li <hello@zhaofeng.li>
2019-06-23 19:02:20 -0700

    ErrorCode.js: Remove extraMessage

--- common/ErrorCode.js
+++ common/ErrorCode.js
@@ -4,15 +4,11 @@ import ErrorCodeInternal from 'static_error_code_internal';
 const result = ErrorCodeInternal.codes;
 const messages = ErrorCodeInternal.messages;
 
-result.getMessage = (code, extraMessage = null) => {
+result.getMessage = (code) => {
   let message = messages[result.UnknownError];
 
   if (Object.prototype.hasOwnProperty.call(messages, code)) {
     message = messages[code];
-  }
-
-  if (extraMessage) {
-    message += `: ${extraMessage}`;
   }
 
   return message;
