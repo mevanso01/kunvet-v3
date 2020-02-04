eecae888d322e248a71e3340e1588e880b950d08
badboybigbill <bz@kunvet.com>
2020-02-04 14:33:23 -0800

    Add customer support email to Contact page

--- client/pages/Contact.vue
+++ client/pages/Contact.vue
@@ -6,6 +6,8 @@
                 <p>If you have any questions or concerns, please don't hesitate to contact us at <a href="mailto:info@kunvet.com">info@kunvet.com</a>.
                 We will respond within a few hours after receiving your email.</p>
                 
+                <p>Need technical support? We're ready to help! All you need to do is send us an email at <a href="mailto:support@kunvet.com">support@kunvet.com</a>.</p>
+                
                 <p>We'd love to hear from you! Send your lovely feedback to our lovely customer service, Rose, at <a href="mailto:rose.lander@kunvet.com">rose.lander@kunvet.com</a>.</p>
                 
                 <p>Not completely satisfied with the service you received? Let us know via <a href="mailto:refund@kunvet.com">refund@kunvet.com</a>, and we'll gladly issue you a full refund immediately.</p>
